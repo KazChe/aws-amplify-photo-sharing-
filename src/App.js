6bce5b1c3eab436db25747faf5982ff84d6c20af
Kam Chehresa <chehresa@amazon.com>
2022-03-29 18:28:55 -0700

    Using latest version of ui-react for adding singout button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import { API } from 'aws-amplify'
 // import query definition
 import { listPosts } from './graphql/queries'
 
-export default function App() {
+//import the withAuthenticator component
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App() {
   const [posts, setPosts] = useState([])
   useEffect(() => {
     fetchPosts();
@@ -33,6 +39,20 @@ export default function App() {
               </div>
           ))
         }
+        <Authenticator>
+            {({ signOut, user }) => (
+                <div className="App">
+                    <p>
+                        Hey {user.username}, welcome to my channel, with auth!
+                    </p>
+                    <button onClick={signOut}>Sign out</button>
+                </div>
+            )}
+        </Authenticator>
       </div>
+
+
   )
 }
+
+export default withAuthenticator(App)
